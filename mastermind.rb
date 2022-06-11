8d71611e78d133520c83953a70f3bb08ff17d96a
T0Rsk1 <tristan_orlowski@hotmail.com>
2022-06-11 12:51:09 -0400

    Create user input methods and hint method

--- mastermind.rb
+++ mastermind.rb
@@ -1,22 +1,60 @@
-class Mastermind
-  TURNS = 12.freeze
-  COLORS = 6.freeze
-  PEGS = 4.freeze
+# frozen_string_literal: true
 
-  attr_reader :code
+# Mastermind
+class Mastermind
+  TURNS = 12
+  COLORS = 6
+  PEGS = 4
+
+  attr_reader :code, :guess
 
   def initialize
     @code = []
+    @guess = []
   end
 
   def mk_code
     PEGS.times { @code << rand(1..COLORS) }
   end
 
-  
+  def input_guess
+    gets.chomp.to_i.digits.reverse
+  end
 
+  def ask_guess
+    puts 'Give me your best shot. Choose 4 numbers between 1 and 6.'
+  end
+
+  def check_guess
+    return true if @guess.size == 4 && @guess.all? { |x| x <= 6 }
+
+    false
+  end
+
+  def wrong_format
+    puts 'Try again. Choose 4 numbers between 1 and 6.'
+  end
+
+  def retrieve_guess
+    ask_guess
+    until check_guess
+      @guess = input_guess
+      wrong_format unless check_guess
+    end
+  end
+
+  def hint
+    hints = []
+    ignore = []
+
+    hints << @guess.zip(@code).count do |x|
+      if x.inject(:eql?)
+        ignore << x[0]
+        true
+      else false
+      end
+    end
+
+    hints << (@guess.uniq - ignore).count { |x| @code.include?(x) }
+  end
 end
-
-game = Mastermind.new
-game.mk_code
-print game.code
