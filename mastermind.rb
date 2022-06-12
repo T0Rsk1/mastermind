5a7989d8e089f2a41523ad4f50f00dede35707d8
T0Rsk1 <tristan_orlowski@hotmail.com>
2022-06-12 15:51:25 -0400

    Create game logic

--- mastermind.rb
+++ mastermind.rb
@@ -22,17 +22,25 @@ class Mastermind
   end
 
   def ask_guess
-    puts 'Give me your best shot. Choose 4 numbers between 1 and 6.'
+    puts 'Give me your best shot! Choose 4 numbers between 1 and 6.'
   end
 
   def check_guess
-    return true if @guess.size == 4 && @guess.all? { |x| x <= 6 }
+    return true if @guess.size == 4 && @guess.all? { |x| x <= 6 && x.positive? }
 
     false
   end
 
   def wrong_format
     puts 'Try again. Choose 4 numbers between 1 and 6.'
+  end
+
+  def end_msg(compare)
+    if compare
+      puts 'You win! You sly dog!'
+    else
+      puts 'Time is up. You suck.'
+    end
   end
 
   def retrieve_guess
@@ -51,10 +59,44 @@ class Mastermind
       if x.inject(:eql?)
         ignore << x[0]
         true
-      else false
       end
     end
 
     hints << (@guess.uniq - ignore).count { |x| @code.include?(x) }
+    to_sym(hints)
+  end
+
+  def to_sym(arr)
+    result = []
+    arr[0].times { result << 'X' }
+    arr[1].times { result << 'O' }
+    result
+  end
+
+  def compare_code
+    @guess == @code
+  end
+
+  def user_input
+    count = 0
+
+    TURNS.times do
+      puts "Turn #{count += 1}"
+      retrieve_guess
+      p hint
+
+      break if compare_code
+
+      @guess = []
+    end
+  end
+
+  def play
+    mk_code
+    # p @code
+    user_input
+    end_msg(compare_code)
   end
 end
+
+Mastermind.new.play
